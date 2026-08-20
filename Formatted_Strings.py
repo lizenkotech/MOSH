2d2dec65f8f34b7daaea65d56f4bc98f2cbd779f
Lizenko <lizenko.official@gmail.com>
2026-08-20 19:02:21 +0200

    Formatted_Strings.py

--- Formatted_Strings.py
+++ Formatted_Strings.py
@@ -3,7 +3,12 @@ last = "Crossman"
 full = first + " " + last
 print(full)"""
 
-first = "Alice"
+"""first = "Alice"
 last = "Crossman"
 full = f"{first} {last}"
+print(full)"""
+
+first = "Alice"
+last = "Crossman"
+full = f"{len(first)} {2+2}"
 print(full)
