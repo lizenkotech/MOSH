0e9d3075f7a4ff2ba0fdb59fb89220719b4b6f0c
Lizenko <lizenko.official@gmail.com>
2026-08-20 20:20:32 +0200

    08Working_With_Numbers.py

--- 08Working_With_Numbers.py
+++ 08Working_With_Numbers.py
@@ -4,3 +4,8 @@ print(round(2.9))
 print(abs(-2.9))
 
 print(math.ceil())
+
+"""google search python3 math module
+> math - mathematical functions - (python 3.7.1rc1)
+documentation
+https://docs.python.org/3/library/math.html"""
