6c09eef44a85279ce9f3a7ea8c4251a0de2e6456
Lizenko <lizenko.official@gmail.com>
2026-08-21 13:07:30 +0200

    16For_else.py

--- 16For_else.py
+++ 16For_else.py
@@ -1,3 +1,10 @@
+# succesful = True
+# for number in range(3):
+#     print("Attempt")
+#     if succesful:
+#         print("Successful")
+#         break
+
 succesful = True
 for number in range(3):
     print("Attempt")
