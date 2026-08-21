fc0be07106ed522a4994909ff7a64c9ebff9ed46
Lizenko <lizenko.official@gmail.com>
2026-08-21 12:46:24 +0200

    13Short-circuit_Evaluations.py

--- 13Short-circuit_Evaluations.py
+++ 13Short-circuit_Evaluations.py
@@ -2,7 +2,7 @@ high_income = False
 good_credit = True
 student = True
 
-if high_income and good_credit and not student:
+if high_income or good_credit or not student:
     print("Eligible")
 else:
     print("Not Eligible")
