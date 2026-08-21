b47e16ddae697080f6784af5ff8d2f14d9b34af8
Lizenko <lizenko.official@gmail.com>
2026-08-21 13:18:23 +0200

    18Iterables.py

---
+++ 18Iterables.py
@@ -0,0 +1,17 @@
+# print(type(5))
+# print(type(range(5)))
+
+# iterable
+# for x in range(5):
+
+
+# for x in "Python":
+#     print(x)
+
+
+# for x in [1, 2, 3, 4]:
+#     print(x)
+
+
+for item in shopping_cart:
+    print(item)
