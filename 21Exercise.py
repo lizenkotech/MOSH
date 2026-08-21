3b3b33ef676a01918f4bff0caf26cfb0323d6bcf
Lizenko <lizenko.official@gmail.com>
2026-08-21 14:12:01 +0200

    21Exercise.py

--- 21Exercise.py
+++ 21Exercise.py
@@ -1,14 +1,4 @@
-# for number in range(1, 10):
-#     if number % 2 == 0:
-#         print(number)
-# print("we have 4 even numbers")
-
-# for number in range(1, 6):
-#     print(number)
-
-for number in range(
-    1,
-    10,
-):
+for number in range(1, 10):
     if number % 2 == 0:
         print(number)
+print("we have 4 even numbers")
