444b616fea634aace83ecb367cb6013548be5e76
Lizenko <lizenko.official@gmail.com>
2026-08-21 12:40:33 +0200

    12Logical_Operator.py

--- 12Logical_Operator.py
+++ 12Logical_Operator.py
@@ -2,8 +2,10 @@
 # or
 # not
 
-high_income = True
+high_income = False
 good_credit = True
 
-if high_income and good_credit:
+if high_income or good_credit:
     print("Eligible")
+else:
+    print("Not Eligible")
