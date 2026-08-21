236870c7daacfaaf8293920b7a96ce145b4584a8
Lizenko <lizenko.official@gmail.com>
2026-08-21 13:04:42 +0200

    15For_Loops.py

---
+++ 15For_Loops.py
@@ -0,0 +1,9 @@
+# for number in range(3):
+#     print("Atttempt", number)
+# -> for loop
+
+# for number in range(3):
+#     print("Atttempt", number + 1, (number + 1) * ".")
+
+for number in range(1, 10, 2):
+    print("Atttempt", number, number * ".")
