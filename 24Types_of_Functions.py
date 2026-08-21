8552d3f2036635d0fb140706308549f85c48129c
Lizenko <lizenko.official@gmail.com>
2026-08-21 14:31:33 +0200

    24Types_of_Functions.py

--- 24Types_of_Functions.py
+++ 24Types_of_Functions.py
@@ -2,16 +2,22 @@ def greet(name):
     print(f"Hi {name}")
 
 
-def get_greeting(name):
-    return f"Hi {name}"
+print(greet("Alice"))
+
+# def greet(name):
+#     print(f"Hi {name}")
 
 
-message = get_greeting("Alice")
-file = open("content.txt", "w")
-file.write(message)
+# def get_greeting(name):
+#     return f"Hi {name}"
 
 
-# 1- function perform a task
-# 2- Return a value
+# message = get_greeting("Alice")
+# file = open("content.txt", "w")
+# file.write(message)
 
-# round(1.9)
+
+# # 1- function perform a task
+# # 2- Return a value
+
+# # round(1.9)
