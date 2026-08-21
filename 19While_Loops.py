1ceb9c9b8ece5e4a9e86ce831e5a4c9f38dd5202
Lizenko <lizenko.official@gmail.com>
2026-08-21 13:26:59 +0200

    19While_Loops.py

--- 19While_Loops.py
+++ 19While_Loops.py
@@ -3,7 +3,18 @@
 #     print(number)
 #     number = number // 2
 
-number = 100
-while number > 0:
-    print(number)
-    number //= 2
+# number = 100
+# while number > 0:
+#     print(number)
+#     number //= 2
+"""
+Python is waiting for an input
+we can type something like
+>>> 2 + 2 it wil evaluate it and ask for the
+next input, we can ad another expression like
+4
+>>> 10 > 2 these steps wil continue until we press
+True
+>>> ^D control D
+~ $
+"""
