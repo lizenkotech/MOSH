c6436d2b1088fc494002741fcda069761620e543
Lizenko <lizenko.official@gmail.com>
2026-08-21 13:12:12 +0200

    17Nested_Loops.py

---
+++ 17Nested_Loops.py
@@ -0,0 +1,14 @@
+# succesful = True
+# for number in range(3):
+#     print("Attempt")
+#     if succesful:
+#         print("Successful")
+#         break
+# for x in range(5):  # ->outer Loop
+#     for y in range(3):  # -> inner Loop
+#         print(f"({x}, {y})")
+
+
+for x in range(5):
+    for y in range(3):
+        print(f"({x}, {y})")
