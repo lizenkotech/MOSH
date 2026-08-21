2e3641558102fc5387aa049b9a81ddcad975e0eb
Lizenko <lizenko.official@gmail.com>
2026-08-21 14:29:23 +0200

    24Types_of_Functions.py

--- 24Types_of_Functions.py
+++ 24Types_of_Functions.py
@@ -1,7 +1,15 @@
 def greet(name):
     print(f"Hi {name}")
 
-    # 1- function perform a task
-    # 2- Return a value
 
-    round(1.9)
+def get_greeting(name):
+    return f"Hi {name}"
+
+
+message = get_greeting("Alice")
+
+
+# 1- function perform a task
+# 2- Return a value
+
+# round(1.9)
