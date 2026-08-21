9a07d349acf4e7f11097d8ab6d02555b0f59172b
Lizenko <lizenko.official@gmail.com>
2026-08-21 14:44:44 +0200

    27xargs.py

--- 27xargs.py
+++ 27xargs.py
@@ -1,6 +1,32 @@
+# def multiply(*numbers):
+#     print(numbers)
+#     # return x * y
+
+
+# multiply(2, 3, 4, 5)
+
+
+# def multiply(*numbers):
+#     for number in numbers:
+#         print(number)
+
+
+# multiply(2, 3, 4, 5)
+
+
+# def multiply(*numbers):
+#     for number in numbers:
+#         total = total * number
+#         total *= number
+
+
+# multiply(2, 3, 4, 5)
+
+
 def multiply(*numbers):
-    print(numbers)
-    # return x * y
+    for number in numbers:
+        total *= number
+    return total
 
 
 multiply(2, 3, 4, 5)
